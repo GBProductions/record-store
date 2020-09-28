91adf9d690b3391649a6f3b2d2770789ad340982
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-28 14:02:22 -0700

    Add more files.

--- app.rb
+++ app.rb
@@ -1,12 +1,62 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/album')
+require('pry')
 also_reload('lib/**/*.rb')
+
+get('/test') do
+  @something = "this is a variable"
+  erb(:whatever)
+end
+
+get('/') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums/new') do
+  # binding.pry
+  erb(:new_album)
+end
+
+post('/albums') do
+  name = params[:album_name]
+  album = Album.new(name, nil)
+  album.save()
+  @albums = Album.all()
+  erb(:albums)
+end
+
+get('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
+end
+
+get('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+end
+
+patch('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.update(params[:name])
+  @albums = Album.all
+  erb(:albums)
+end
 
 class Album
   attr_reader :name
-  ...
+
   def ==(other_album)
     self.name.eql?(other_album.name) && self.artist.eql?(other_album.artist) && self.year.eql?(other_album.year)
   end
+
+
+
 end
 
